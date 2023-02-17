b0defbaed3b3f40ce7dc80d9142ce31a7c0f9a4c
Nemes1sX <kazstonk@gmail.com>
2023-02-17 23:47:23 +0200

    Added data context

---
+++ ChildrenGift/DatContext/ChildGiftContext.cs
@@ -0,0 +1,20 @@
+﻿using ChildrenGift.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildrenGift.DatContext
+{
+    public class ChildGiftContext : DbContext
+    {
+        public ChildGiftContext(DbContextOptions<ChildGiftContext> contextOptions) : base(contextOptions) 
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+    }
+}
---
+++ ChildrenGift/DatContext/ChildrenGiftSeeding.cs
@@ -0,0 +1,31 @@
+﻿using ChildrenGift.Models.Entities;
+using FizzWare.NBuilder;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ChildrenGift.DatContext
+{
+    public static class ChildrenGiftSeeding
+    {
+        public static void SeedData(ModelBuilder modelBuilder)
+        {
+            Random rnd = new Random();
+            var childern = Builder<Child>.CreateListOfSize(100)
+                .All()
+                .With(m => m.FirstName = Faker.Name.First())
+                .With(m => m.LastName = Faker.Name.Last())
+                .Build();
+
+            modelBuilder.Entity<Child>().HasData(childern);
+
+            var gift = Builder<Gift>.CreateListOfSize(250)
+                .All()
+                .With(m => m.Name = Faker.Lorem.GetFirstWord())
+                .With(m => m.GiftId = rnd.Next(1, 100))
+                .Build();
+
+            modelBuilder.Entity<Gift>().HasData(gift);
+
+        }
+    }
+}
