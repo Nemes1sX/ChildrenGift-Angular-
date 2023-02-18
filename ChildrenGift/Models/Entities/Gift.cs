15716d433c44bfc214f516bf853f0c4a5e77a3ec
Nemes1sX <kazstonk@gmail.com>
2023-02-18 14:43:49 +0200

    Fix migrations and foreign key

--- ChildrenGift/Models/Entities/Gift.cs
+++ ChildrenGift/Models/Entities/Gift.cs
@@ -1,9 +1,12 @@
-﻿namespace ChildrenGift.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChildrenGift.Models.Entities
 {
     public class Gift : Entity
     {
         public string Name { get; set; }
         public int ChildId { get; set; }
+        [ForeignKey("ChildId")]
         public virtual Child Child { get; set; }
     }
 }
